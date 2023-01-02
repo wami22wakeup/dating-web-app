9ae9a0095bc1702c6b9e243427a5ea7240f663a2
Divyanshu Prasad <90151736+Divyanshu9822@users.noreply.github.com>
2023-01-02 19:46:58 +0530

    Project setup

---
+++ dating-web-app/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>dating app</h1>
+    </div>
+  );
+}
+
+export default App;
--- test.py
+++
@@ -1,9 +0,0 @@
-print("hello world")
-print("hello world")
-print("hello world")
-
-for i in range(5):
-    print("*"*5)
-    
-for i in range(5):
-    print("*"*5)
